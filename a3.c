3f272adaa02ccbd684c98ca9a6bfd9537c818c27
Bucksin <bucksin8@gmail.com>
2022-12-01 14:45:11 -0800

    fixed compaction to avoid freeing nodes when the list is just holes.

--- a3.c
+++ a3.c
@@ -1,55 +1,93 @@
-//
-// Created by Princeton Dychinco on 2022-11-27.
-//
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include "a3.h"
 
-void frontBackSplit(nodeptr source, nodeptr* frontRef, nodeptr* backRef) {
+/**
+ * File:    a3.c
+ *
+ * Author: Buck Sin - A00805677
+ * Author: Brandon Chan - A01026254
+ * Author: Princeton Dychinco - A00849214
+ * Author: Stanley Chow - A01266659
+ *
+ * COMP 2510: Set B
+ * Date: Nov 30, 2022
+ *
+ * Summary of File:
+ *
+ *      This program opens an interactive menu that allows the user
+ *      to select their choice from 5 options. The options available
+ *      to the user include: loading an input file, merging adjacent
+ *      holes in memory, compacting memory, printing the memory view
+ *      and terminating the program.
+ */
+
+
+/**
+ * Splits a linkedlist into front and back halves.
+ *
+ * @param source head of the linkedlist to be split
+ * @param frontRef front half of linkedlist
+ * @param backRef back half of linkedlist
+ */
+void frontBackSplit(nodeptr source, nodeptr *frontRef, nodeptr *backRef) {
   nodeptr fast;
   nodeptr slow;
   slow = source;
-  fast = source -> next;
+  fast = source->next;
 
   while (fast != NULL) {
-    fast = fast -> next;
+    fast = fast->next;
     if (fast != NULL) {
-      slow = slow -> next;
-      fast = fast -> next;
+      slow = slow->next;
+      fast = fast->next;
     }
   }
 
   *frontRef = source;
-  *backRef = slow -> next;
-  slow -> next = NULL;
+  *backRef = slow->next;
+  slow->next = NULL;
 }
 
+/**
+ * Compares two nodes and returns the one with the lower base.
+ *
+ * @param a first node
+ * @param b second node
+ * @return pointer to the node with lower base. Returns the other if
+ * one of them is null.
+ */
 nodeptr sortedMerge(nodeptr a, nodeptr b) {
   nodeptr result = NULL;
 
-  if(a == NULL) {
+  if (a == NULL) {
     return b;
   } else if (b == NULL) {
     return a;
   }
 
-  if(a -> base <= b -> base) {
+  if (a->base <= b->base) {
     result = a;
-    result -> next = sortedMerge(a->next, b);
+    result->next = sortedMerge(a->next, b);
   } else {
     result = b;
-    result -> next = sortedMerge(a, b-> next);
+    result->next = sortedMerge(a, b->next);
   }
   return result;
 }
 
-void mergeSort(nodeptr* headRef) {
+/**
+ * Performs a merge sort on a linkedlist.
+ *
+ * @param headRef head of the linkedlist to be sorted
+ */
+void mergeSort(nodeptr *headRef) {
   nodeptr head = *headRef;
   nodeptr a;
   nodeptr b;
 
-  if((head == NULL) || head->next == NULL) {
+  if ((head == NULL) || head->next == NULL) {
     return;
   }
 
@@ -57,134 +95,247 @@ void mergeSort(nodeptr* headRef) {
   mergeSort(&a);
   mergeSort(&b);
 
-  *headRef = sortedMerge(a,b);
+  *headRef = sortedMerge(a, b);
 }
 
+/**
+ * Creates an empty linkedlist.
+ *
+ * @return a pointer to the newly created linkedlist
+ */
 llptr createLinkedList() {
   llptr linkList = malloc(sizeof(llptr));
-  linkList -> head = NULL;
-  linkList -> tail = NULL;
-  linkList -> numOfNodes = 0;
+  linkList->head = NULL;
+  linkList->tail = NULL;
+  linkList->numOfNodes = 0;
   return linkList;
 }
 
+/**
+ * Adds a node to a linkedlist.
+ *
+ * @param linkList to be appended
+ * @param node pointer to the node being added
+ */
 void addToLinkedList(llptr linkList, nodeptr node) {
   if (linkList->head == NULL) {
-    linkList -> head = node;
-    linkList -> tail = node;
-    linkList -> numOfNodes = 1;
+    linkList->head = node;
+    linkList->tail = node;
+    linkList->numOfNodes = 1;
   } else {
-    linkList -> tail -> next = node;
-    linkList -> tail = node;
-    linkList -> numOfNodes++;
+    linkList->tail->next = node;
+    linkList->tail = node;
+    linkList->numOfNodes++;
   }
 }
 
-void saveString(nodeptr node, char* str) {
-  for(int i = 0; i < strlen(str); i++) {
-    node -> identifier[i] = str[i];
-  }
-}
-
-nodeptr createNode(char** str) {
+/**
+ * Creates a node representing a block of memory from a string.
+ *
+ * @param str from which the node will be created
+ * @return pointer to a newly created node
+ */
+nodeptr createNode(char **str) {
   nodeptr node = (nodeptr) malloc(sizeof(struct Node));
-  saveString(node, str[0]);
-  node -> base = atoi(str[1]);
-  node -> limit = atoi(str[2]);
+  strcpy(node->identifier, str[0]);
+  node->base = atoi(str[1]);
+  node->limit = atoi(str[2]);
+  node->next = NULL;
   return node;
 }
 
-void readFile(FILE* fptr, llptr linkList) {
+/**
+ * Reads an input file. creates nodes for each block of memory,
+ * and stores a sorted linkedlist.
+ *
+ * @param fptr input text file
+ * @param linkList storing the sorting blocks of memory
+ */
+void readFile(FILE *fptr, llptr linkList) {
   char buffer[100];
-  while(fgets(buffer, 100, fptr) != NULL) {
-    char** splitString = (char**)malloc(sizeof(char*) * 10);
-    char* token;
+  while (fgets(buffer, 100, fptr) != NULL) {
+    char **splitString = (char **) malloc(sizeof(char *) * 10);
+    char *token;
     int counter = 0;
     token = strtok(buffer, " ");
-    while(token != NULL) {
-      splitString[counter] = (char*)malloc(sizeof(char) * 10);
+    while (token != NULL) {
+      splitString[counter] = (char *) malloc(sizeof(char) * 10);
       splitString[counter] = token;
       counter++;
-      token = strtok(NULL," ");
+      token = strtok(NULL, " ");
     }
     addToLinkedList(linkList, createNode(splitString));
   }
-  mergeSort(&(linkList -> head));
+  mergeSort(&(linkList->head));
+
 }
 
-void printMemory(llptr linkList) {
-  nodeptr node = linkList -> head;
-  int counter = 1;
-  while(node != NULL) {
-    if(strcmp(node->identifier, "H") == 0) {
-      printf("Node %d: %s (Hole), base=%d, limit=%d\n",counter, node->identifier, node->base, node->limit);
-    } else {
-      printf("Node %d: %s, base=%d, limit=%d\n",counter, node->identifier, node->base, node->limit);
-    }
-    counter++;
-    node = node -> next;
-  }
-}
+/**
+ * Function checks to see if the created linked list has the valid properties after first sort.
+ * @param linkedList storing the sorting blocks of memory
+ */
+void linkedListCheck(llptr linkedList) {
+    nodeptr currentNode = linkedList -> head;
 
-void mergeHoles(llptr linkList) {
-  printf("2 segfault");
-  nodeptr node = linkList -> head;
-  int counter = 3;
-  while(node != NULL) {
-    counter++;
-    nodeptr nextNode = node -> next;
-    if(strcmp(node -> identifier, "H") == 0 && strcmp(nextNode -> identifier, "H") == 0) {
-      node -> limit += nextNode -> limit;
-      node -> next = nextNode -> next;
-    } else {
-      node = node -> next;
-    }
-  }
-}
-
-int main() {
-  int input;
-  int fileNotRead = 1;
-  llptr linkList = createLinkedList();
-  do{
-    printf("1. load an input file\n2. merge holes\n3. compact memory\n4. print memory view\n5. exit the program\n");
-    scanf("%d", &input);
-    switch(input) {
-      case 1: {
-        if(fileNotRead) {
-          fileNotRead = 0;
-          printf("1. Type the file name: ");
-          char* fileName = malloc(sizeof(char) * 20);
-          scanf("%s", fileName);
-          FILE* fptr = fopen(fileName, "r");
-          readFile(fptr, linkList);
-          printf("operation successful\n");
-          break;
-        } else {
-          printf("ERROR: You have already read a file. Please exit the program to read another file.\n");
-          break;
+    while (currentNode -> next != NULL) {
+        if (currentNode -> base + currentNode -> limit != currentNode -> next -> base) {
+            printf("Error: Input file did not have valid properties.");
+            exit(1);
         }
-      }
-      case 2: {
-        printf("1 segfault\n");
-        mergeHoles(linkList);
-        printf("operation successful\n");
-        break;
-      }
-      case 3: {
-        printf("compacting memory");
-        printf("operation successful\n");
-        break;
-      }
-      case 4: {
-        printMemory(linkList);
-        break;
-      }
-      case 5: {
-        printf("good bye");
-        break;
-      }
+        currentNode = currentNode -> next;
     }
-  }while(input !=5);
+}
+
+/**
+ * Prints the view from memory. Blocks of memory are separated by lines.
+ *
+ * @param linkList containing blocks of memory
+ */
+void printMemory(llptr linkList) {
+  nodeptr node = linkList->head;
+  int counter = 1;
+  while (node != NULL) {
+    if (strcmp(node->identifier, "H") == 0) {
+      printf("Node %d: %s (Hole), base=%d, limit=%d\n", counter, node->identifier, node->base, node->limit);
+    } else {
+      printf("Node %d: %s, base=%d, limit=%d\n", counter, node->identifier, node->base, node->limit);
+    }
+    counter++;
+    node = node->next;
+  }
+}
+
+/**
+ * Merges adjacent holes of free memory to avoid fragmentation.
+ *
+ * @param linkList containing blocks of memory
+ */
+void mergeHoles(llptr linkList) {
+  nodeptr node = linkList->head;
+  int counter = 3;
+  while (node->next != NULL) {
+    counter++;
+    nodeptr nextNode = node->next;
+    if (strcmp(node->identifier, "H") == 0 && strcmp(nextNode->identifier, "H") == 0) {
+      node->limit += nextNode->limit;
+      node->next = nextNode->next;
+    } else {
+      node = node->next;
+    }
+  }
+}
+
+/**
+ * Combines the non-process hole nodes into one node and places it at the end of the linkedlist.
+ *
+ * @param linkList data structure with the process data
+ */
+void compactMemory(llptr linkList) {
+  nodeptr node = linkList->head;
+  nodeptr prevProcessNode = NULL;
+  nodeptr holeNode = NULL;
+  int limitCounter = 0;
+
+  while (node != NULL) {
+    if (strcmp(node->identifier, "H") == 0) {
+      holeNode = node;
+      limitCounter += node->limit;
+      node = node->next;
+      if(prevProcessNode != NULL) {
+          free(holeNode);
+      }
+    } else {
+      if (prevProcessNode != NULL) {
+        prevProcessNode->next = node;
+        node->base = prevProcessNode->base + prevProcessNode->limit;
+      } else {
+        linkList -> head = node;
+        node->base = 0;
+      }
+      prevProcessNode = node;
+      linkList->tail = node;
+      node = node->next;
+    }
+  }
+  if(limitCounter > 0) {
+      nodeptr newHole = (nodeptr) malloc(sizeof(struct Node));
+      strcpy(newHole->identifier, "H");
+      newHole->base = linkList->tail->base + linkList->tail->limit;
+      newHole->limit = limitCounter;
+      newHole->next = NULL;
+      addToLinkedList(linkList, newHole);
+  }
+}
+
+/**
+ * Contains conditional logic to execute the commands the user wishes through the menu. Contains
+ * multiple options to load an input file, merge holes, compact memory and print the memory view.
+ *
+ * @param fileNotRead
+ * @param linkedList data structure with the process data
+ */
+void menu(llptr linkedList) {
+    int input;
+    int fileNotRead = 1;
+    do {
+        printf("1. load an input file\n2. merge holes\n3. compact memory\n4. print memory view\n5. exit the program\n");
+        scanf("%d", &input);
+        switch (input) {
+            case 1: {
+                if (fileNotRead) {
+                    fileNotRead = 0;
+                    printf("1. Type the file name: ");
+                    char *fileName = malloc(sizeof(char) * 20);
+                    scanf("%s", fileName);
+                    FILE *fptr = fopen(fileName, "r");
+
+                    if (fptr == NULL) {
+                        printf("Error: file could not be found.");
+                        exit(1);
+                    }
+                    readFile(fptr, linkedList);
+                    linkedListCheck(linkedList);
+                    printf("operation successful\n");
+                    break;
+                } else {
+                    printf("ERROR: You have already read a file. Please exit the program to read another file.\n");
+                    break;
+                }
+            }
+            case 2: {
+                mergeHoles(linkedList);
+                printf("operation successful\n");
+                break;
+            }
+            case 3: {
+                compactMemory(linkedList);
+                printf("operation successful\n");
+                break;
+            }
+            case 4: {
+                printMemory(linkedList);
+                break;
+            }
+            case 5: {
+                printf("good bye");
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+    } while (input != 5);
+}
+
+/**
+ * Main function that controls the option menu. Nested calls are
+ * made based on the user's input.
+ *
+ * @return 0 if execution runs without error
+ */
+int main() {
+  llptr linkList = createLinkedList();
+
+  menu(linkList);
   return 0;
 }
