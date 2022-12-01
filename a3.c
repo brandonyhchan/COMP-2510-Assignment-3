aee06dfcadf790dc9aafbdcfb8eab2f771ea6363
pdychinco <prdychinco@gmail.com>
2022-11-30 19:38:24 -0800

    fixed merge and compaction

--- a3.c
+++ a3.c
@@ -1,55 +1,52 @@
-//
-// Created by Princeton Dychinco on 2022-11-27.
-//
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include "a3.h"
 
-void frontBackSplit(nodeptr source, nodeptr* frontRef, nodeptr* backRef) {
+void frontBackSplit(nodeptr source, nodeptr *frontRef, nodeptr *backRef) {
   nodeptr fast;
   nodeptr slow;
   slow = source;
-  fast = source -> next;
+  fast = source->next;
 
   while (fast != NULL) {
-    fast = fast -> next;
+    fast = fast->next;
     if (fast != NULL) {
-      slow = slow -> next;
-      fast = fast -> next;
+      slow = slow->next;
+      fast = fast->next;
     }
   }
 
   *frontRef = source;
-  *backRef = slow -> next;
-  slow -> next = NULL;
+  *backRef = slow->next;
+  slow->next = NULL;
 }
 
 nodeptr sortedMerge(nodeptr a, nodeptr b) {
   nodeptr result = NULL;
 
-  if(a == NULL) {
+  if (a == NULL) {
     return b;
   } else if (b == NULL) {
     return a;
   }
 
-  if(a -> base <= b -> base) {
+  if (a->base <= b->base) {
     result = a;
-    result -> next = sortedMerge(a->next, b);
+    result->next = sortedMerge(a->next, b);
   } else {
     result = b;
-    result -> next = sortedMerge(a, b-> next);
+    result->next = sortedMerge(a, b->next);
   }
   return result;
 }
 
-void mergeSort(nodeptr* headRef) {
+void mergeSort(nodeptr *headRef) {
   nodeptr head = *headRef;
   nodeptr a;
   nodeptr b;
 
-  if((head == NULL) || head->next == NULL) {
+  if ((head == NULL) || head->next == NULL) {
     return;
   }
 
@@ -57,88 +54,87 @@ void mergeSort(nodeptr* headRef) {
   mergeSort(&a);
   mergeSort(&b);
 
-  *headRef = sortedMerge(a,b);
+  *headRef = sortedMerge(a, b);
 }
 
 llptr createLinkedList() {
   llptr linkList = malloc(sizeof(llptr));
-  linkList -> head = NULL;
-  linkList -> tail = NULL;
-  linkList -> numOfNodes = 0;
+  linkList->head = NULL;
+  linkList->tail = NULL;
+  linkList->numOfNodes = 0;
   return linkList;
 }
 
 void addToLinkedList(llptr linkList, nodeptr node) {
   if (linkList->head == NULL) {
-    linkList -> head = node;
-    linkList -> tail = node;
-    linkList -> numOfNodes = 1;
+    linkList->head = node;
+    linkList->tail = node;
+    linkList->numOfNodes = 1;
   } else {
-    linkList -> tail -> next = node;
-    linkList -> tail = node;
-    linkList -> numOfNodes++;
+    linkList->tail->next = node;
+    linkList->tail = node;
+    linkList->numOfNodes++;
   }
 }
 
-void saveString(nodeptr node, char* str) {
-  for(int i = 0; i < strlen(str); i++) {
-    node -> identifier[i] = str[i];
+void saveString(nodeptr node, char *str) {
+  for (int i = 0; i < strlen(str); i++) {
+    node->identifier[i] = str[i];
   }
 }
 
-nodeptr createNode(char** str) {
+nodeptr createNode(char **str) {
   nodeptr node = (nodeptr) malloc(sizeof(struct Node));
   strcpy(node->identifier, str[0]);
-  node -> base = atoi(str[1]);
-  node -> limit = atoi(str[2]);
-  node -> next = NULL;
+  node->base = atoi(str[1]);
+  node->limit = atoi(str[2]);
+  node->next = NULL;
   return node;
 }
 
-void readFile(FILE* fptr, llptr linkList) {
+void readFile(FILE *fptr, llptr linkList) {
   char buffer[100];
-  while(fgets(buffer, 100, fptr) != NULL) {
-    char** splitString = (char**)malloc(sizeof(char*) * 10);
-    char* token;
+  while (fgets(buffer, 100, fptr) != NULL) {
+    char **splitString = (char **) malloc(sizeof(char *) * 10);
+    char *token;
     int counter = 0;
     token = strtok(buffer, " ");
-    while(token != NULL) {
-      splitString[counter] = (char*)malloc(sizeof(char) * 10);
+    while (token != NULL) {
+      splitString[counter] = (char *) malloc(sizeof(char) * 10);
       splitString[counter] = token;
       counter++;
-      token = strtok(NULL," ");
+      token = strtok(NULL, " ");
     }
     addToLinkedList(linkList, createNode(splitString));
   }
-//  mergeSort(&(linkList -> head));
+  mergeSort(&(linkList->head));
 }
 
 void printMemory(llptr linkList) {
-  nodeptr node = linkList -> head;
+  nodeptr node = linkList->head;
   int counter = 1;
-  while(node != NULL) {
-    if(strcmp(node->identifier, "H") == 0) {
-      printf("Node %d: %s (Hole), base=%d, limit=%d\n",counter, node->identifier, node->base, node->limit);
+  while (node != NULL) {
+    if (strcmp(node->identifier, "H") == 0) {
+      printf("Node %d: %s (Hole), base=%d, limit=%d\n", counter, node->identifier, node->base, node->limit);
     } else {
-      printf("Node %d: %s, base=%d, limit=%d\n",counter, node->identifier, node->base, node->limit);
+      printf("Node %d: %s, base=%d, limit=%d\n", counter, node->identifier, node->base, node->limit);
     }
     counter++;
-    node = node -> next;
+    node = node->next;
   }
 }
 
 void mergeHoles(llptr linkList) {
-  printf("2 segfault");
-  nodeptr node = linkList -> head;
+  nodeptr node = linkList->head;
   int counter = 3;
-  while(node != NULL) {
+  while (node->next != NULL) {
     counter++;
-    nodeptr nextNode = node -> next;
-    if(strcmp(node -> identifier, "H") == 0 && strcmp(nextNode -> identifier, "H") == 0) {
-      node -> limit += nextNode -> limit;
-      node -> next = nextNode -> next;
+    nodeptr nextNode = node->next;
+    if (strcmp(node->identifier, "H") == 0 && strcmp(nextNode->identifier, "H") == 0) {
+      node->limit += nextNode->limit;
+      node->next = nextNode->next;
     } else {
-      node = node -> next;
+      node = node->next;
     }
   }
 }
@@ -148,49 +144,50 @@ void mergeHoles(llptr linkList) {
  * @param linkList data structure with the process data
  */
 void compactMemory(llptr linkList) {
-    nodeptr node = linkList -> head;
-    nodeptr prevProcessNode = NULL;
-    int limitCounter = 0;
+  nodeptr node = linkList->head;
+  nodeptr prevProcessNode = NULL;
+  int limitCounter = 0;
 
-    while(node != NULL) {
-        if(strcmp(node -> identifier, "H") == 0) {
-            limitCounter += node -> limit;
-            node = node -> next;
-        } else {
-            if(prevProcessNode != NULL) {
-                prevProcessNode->next = node;
-                node->base = prevProcessNode->base + prevProcessNode->limit;
-            } else {
-                node->base = 0;
-            }
-            prevProcessNode = node;
-            linkList->tail = node;
-            node = node -> next;
-        }
+  while (node != NULL) {
+    if (strcmp(node->identifier, "H") == 0) {
+      limitCounter += node->limit;
+      node = node->next;
+    } else {
+      if (prevProcessNode != NULL) {
+        prevProcessNode->next = node;
+        node->base = prevProcessNode->base + prevProcessNode->limit;
+      } else {
+        linkList -> head = node;
+        node->base = 0;
+      }
+      prevProcessNode = node;
+      linkList->tail = node;
+      node = node->next;
     }
-    nodeptr newHole = (nodeptr) malloc(sizeof(struct Node));
-    strcpy(newHole->identifier, "H");
-    newHole->base = linkList->tail->base + linkList->tail->limit;
-    newHole->limit = limitCounter;
-    newHole->next = NULL;
-    addToLinkedList(linkList, newHole);
+  }
+  nodeptr newHole = (nodeptr) malloc(sizeof(struct Node));
+  strcpy(newHole->identifier, "H");
+  newHole->base = linkList->tail->base + linkList->tail->limit;
+  newHole->limit = limitCounter;
+  newHole->next = NULL;
+  addToLinkedList(linkList, newHole);
 }
 
 int main() {
   int input;
   int fileNotRead = 1;
   llptr linkList = createLinkedList();
-  do{
+  do {
     printf("1. load an input file\n2. merge holes\n3. compact memory\n4. print memory view\n5. exit the program\n");
     scanf("%d", &input);
-    switch(input) {
+    switch (input) {
       case 1: {
-        if(fileNotRead) {
+        if (fileNotRead) {
           fileNotRead = 0;
           printf("1. Type the file name: ");
-          char* fileName = malloc(sizeof(char) * 20);
+          char *fileName = malloc(sizeof(char) * 20);
           scanf("%s", fileName);
-          FILE* fptr = fopen(fileName, "r");
+          FILE *fptr = fopen(fileName, "r");
           readFile(fptr, linkList);
           printf("operation successful\n");
           break;
@@ -200,13 +197,11 @@ int main() {
         }
       }
       case 2: {
-        printf("1 segfault\n");
         mergeHoles(linkList);
         printf("operation successful\n");
         break;
       }
       case 3: {
-        printf("compacting memory");
         compactMemory(linkList);
         printf("operation successful\n");
         break;
@@ -220,6 +215,6 @@ int main() {
         break;
       }
     }
-  }while(input !=5);
+  } while (input != 5);
   return 0;
 }
